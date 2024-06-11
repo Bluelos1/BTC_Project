9c3da236d3b51e7855559a83db49de54782aa4b5
Mateusz Placha <s25946@pjwstk.edu.pl>
2024-06-11 14:04:36 +0200

    Add bitcoin value out of transaction

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 from config import RPC_USER, RPC_PASSWORD, RPC_HOST, RPC_PORT
 from graphviz import Digraph
 
-MAX_DEPTH = 50
+MAX_DEPTH = 10
 
 def get_rpc_connection():
     rpc_url = f"http://{RPC_USER}:{RPC_PASSWORD}@{RPC_HOST}:{RPC_PORT}"
@@ -18,19 +18,23 @@ def fetch_transaction(txid):
         print(f"Unexpected error fetching transaction {txid}: {e}")
     return None
 
-def build_transaction_tree(txid, depth=0):
+def build_transaction_tree(txid, depth=0, incoming_value=0):
     transaction = fetch_transaction(txid)
     if not transaction or 'vin' not in transaction or not transaction['vin']:
         return {"txid": txid, "inputs": [], "coinbase": True, "details": transaction}
 
-    tree = {"txid": txid, "inputs": [], "details": transaction}
+    tree = {"txid": txid, "inputs": [], "details": transaction, "incoming_value": incoming_value}
     if depth < MAX_DEPTH:
         for vin in transaction.get('vin', []):
             child_txid = vin.get('txid')
+            vout_index = vin.get('vout')
             if child_txid:
-                child_tree = build_transaction_tree(child_txid, depth + 1)
-                if child_tree:
-                    tree['inputs'].append(child_tree)
+                parent_transaction = fetch_transaction(child_txid)
+                if 'vout' in parent_transaction and len(parent_transaction['vout']) > vout_index:
+                    child_value = float(parent_transaction['vout'][vout_index]['value'])
+                    child_tree = build_transaction_tree(child_txid, depth + 1, child_value)
+                    if child_tree:
+                        tree['inputs'].append(child_tree)
     return tree
 
 def visualize_transaction_tree(tree):
@@ -48,8 +52,7 @@ def add_nodes_and_edges(tree, dot, parent=None):
 
     dot.node(tree['txid'], label, shape='rect', style='filled', color='darkgoldenrod2')
     if parent:
-        dot.edge(tree['txid'], parent, color='black')
-
+        dot.edge(tree['txid'], parent, label=f"{tree['incoming_value']} BTC", color='black')
     for input_tree in tree['inputs']:
         add_nodes_and_edges(input_tree, dot, tree['txid'])
 
